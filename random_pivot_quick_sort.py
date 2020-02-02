df057753ac0af060153b6ca879c5e51ebc1d3a93
oh980225 <oh980225@gmail.com>
2020-02-02 12:58:56 +0000

    Add random_pivot_quick_sort

---
+++ random_pivot_quick_sort.py
@@ -0,0 +1,16 @@
+from random import randint
+
+def quick_sort(a, start, end):
+  if end - start <= 0:
+    return
+  ran = randint(start, end)
+  pivot = a[ran]
+  a[end], a[ran] = a[ran], a[end]
+  i = start
+  for j in range(start, end):
+    if a[j] <= pivot:
+      a[i], a[j] = a[j], a[i]
+      i += 1
+  a[i], a[end] = a[end], a[i]
+  quick_sort(a, start, i -1)
+  quick_sort(a, i + 1, end)
